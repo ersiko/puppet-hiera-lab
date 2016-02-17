13b0d7690007d31b6d1ee6a0eb92a602e993fa23
Tomas Nunez <tomas@criptos.com>
2016-02-17 01:24:00 +0100

    Preparing first step of the lab demo

--- puppet-config/modules/facts/lib/facter/facts.rb
+++ puppet-config/modules/facts/lib/facter/facts.rb
@@ -1,20 +1,5 @@
-Facter.add('role') do
-  setcode do
-    hostname = Facter.value('hostname')
-    match = hostname.match /^(\D+)/
-    role = match.captures[0]
-    role
-  end
-end
-
 Facter.add("env") do
   setcode do
     Facter.value('fqdn').split('.')[-3]
-  end
-end
-
-Facter.add("datacenter") do
-  setcode do
-    Facter.value('fqdn').split('.')[-4]
   end
 end
--- puppet-config/modules/webpage/manifests/init.pp
+++
@@ -1,5 +0,0 @@
-class webpage() {
-  file { '/var/www/html/index.html':
-    content => template('webpage/index.html.erb')
-  }
-}
