57b734cd77c3af962cd5a1ab3145b417f153f657
Tomas Nunez <tomas@criptos.com>
2016-02-17 01:48:45 +0100

    Preparing 3rd step of the lab

--- puppet-config/modules/facts/lib/facter/facts.rb
+++ puppet-config/modules/facts/lib/facter/facts.rb
@@ -1,12 +1,3 @@
-Facter.add('role') do
-  setcode do
-    hostname = Facter.value('hostname')
-    match = hostname.match /^(\D+)/
-    role = match.captures[0]
-    role
-  end
-end
-
 Facter.add("env") do
   setcode do
     Facter.value('fqdn').split('.')[-3]
